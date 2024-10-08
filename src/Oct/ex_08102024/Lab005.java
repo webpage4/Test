7ef062f767741b09345c4456ce1084ec9a540d74
Test <ahbadge@gmail.com>
2024-10-08 06:57:38 +0530

    Commit ammended of 8

---
+++ src/Oct/ex_08102024/Lab005.java
@@ -0,0 +1,15 @@
+package Oct.ex_08102024;
+
+public class Lab005 {
+    public static void main(String[] args) {
+        int age = 10;
+
+        System.out.println(age);
+        float f = 12;
+        System.out.println(f);
+        // int -> data type - whole no(no decimal)
+        //age -> variable_name - identifier
+        // = ->literal/variable_value
+
+    }
+}
