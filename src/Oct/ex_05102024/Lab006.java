74cc015917cebf9243e90b0384e457c5be8d004e
Test <ahbadge@gmail.com>
2024-10-11 07:16:44 +0530

    yes

--- src/Oct/ex_05102024/Lab006.java
+++
@@ -1,7 +0,0 @@
-package Oct.ex_05102024;
-
-public class Lab006 {
-    public static void main(String[] args) {
-        System.out.println("this is for git purpose - commit and push");
-    }
-}
--- src/Oct/ex_05102024/git.java
+++
@@ -1,7 +0,0 @@
-package Oct.ex_05102024;
-
-public class git {
-    public static void main(String[] args) {
-        System.out.println("123");
-    }
-}
